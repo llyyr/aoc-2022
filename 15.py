f26b767ec4c68bd514faa2192b093cc52c1ed3ff
llyyr <llyyr.public@gmail.com>
2022-12-15 12:55:31 +0530

    how to z3

--- 15.py
+++ 15.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.11
 
 from aochelper import *
-from z3 import Or, Ints, Solver, sat
+from z3 import If, Ints, Solver, sat
 
 def solve(p2=4000000, p1=2000000):
     S = set()
@@ -12,6 +12,7 @@ def solve(p2=4000000, p1=2000000):
     s.add(x <= p2)
     s.add(0 <= y)
     s.add(y <= p2)
+    Zabs = lambda x: If(x < 0, -x, x)
     for line in open('15.txt').read().splitlines():
         sx, sy, bx, by = ints(line)
         dist = abs(sx - bx) + abs(sy - by)
@@ -20,10 +21,7 @@ def solve(p2=4000000, p1=2000000):
             B.add(bx)
         for sensor in range(sx - lim, sx + lim + 1):
             S.add(sensor)
-        s.add(Or((x + y > dist + sx + sy),
-                 (x + y < -dist + sx + sy),
-                 (x - y > dist + sx - sy),
-                 (x - y < -dist + sx - sy)))
+        s.add(Zabs(x - sx) + Zabs(y - sy) > dist)
 
     assert s.check() == sat
     m = s.model()
